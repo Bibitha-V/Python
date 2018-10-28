be571eadc4036db866648820be6ff3759d51f05a
Bibitha <bibithav11@gmail.com>
2018-10-28 15:26:33 +0530

    First Program

---
+++ ftprog.py
@@ -0,0 +1,4 @@
+print("hello world")
+var1=input("Enter your name")
+print(var1)
+print("hello world",var1)
---
+++ simple interest.py
@@ -0,0 +1,5 @@
+pa=int(input("Enter the Principal amount:"))
+ir=float(input("Enter the interest rate %:"))
+t=int(input("Enter the time period:"))
+si=pa*ir/100*t
+print(si)
